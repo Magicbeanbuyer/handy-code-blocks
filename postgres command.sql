d7321ae771aa53e9d53ff9283c33730eb72b04d7
xiatong <vanessazxt@gmail.com>
2020-07-23 09:30:15 +0200

    added grand grant query

--- postgres command.sql
+++ postgres command.sql
@@ -1,5 +1,6 @@
 GRANT USAGE ON SCHEMA schema_name TO user_name;
 GRANT SELECT ON ALL TABLES IN SCHEMA schema_name TO user_name;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA schema_name TO user_name;
 
 REVOKE SELECT ON table_name FROM user_name;
 
