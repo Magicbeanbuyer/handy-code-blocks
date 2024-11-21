07444a6639427e39c712d58cee932db240eb888a
Xiatong Zheng <xiatong.zheng@deepl.com>
2024-11-21 11:46:12 +0100

    save

---
+++ bash_commands.sh
@@ -0,0 +1,18 @@
+# search for strings in a folder
+grep -r 'pattern_to_match' directory_to_search
+
+# to unlock locked folders
+sudo chown -R $USER: $HOME
+
+# SSH Key Generation
+ssh-keygen -t rsa -b 4096 -C "neodigital claim SFPT" -f ./rsa-key
+
+# copy all files from one dir to another 
+cp -a from/. to/
+
+# remove a dir completely
+rm -rf to
+
+# list file size, -h, --human-readable
+ls -lh
+
--- git commands.sh
+++ git commands.sh
@@ -1,48 +1,26 @@
-#################################
-## push local branch to remote ##
-#################################
-# Create a new branch:
-git checkout -b feature_branch_name
-
-# stage and commit your changes
+# stage all tracked files and commit your changes
 git commit -am "your commit message"
+# stage all files, both tracked and untracked, and commit your changes
+git add -A && git commit -m "save"
+
+# empty git commit
+git commit --allow-empty -m ""
 
 # Push your branch to the remote repository:
 git push --set-upstream origin feature_branch_name
 
-
-###########################
-## push to remote master ##
-###########################
-git push origin master
-
-
-#################################
-## pull from remote repository ##
-#################################
+# update local repo
 git pull --all
 
-
-##########################
-## delete merged branch ##
-##########################
 # prune
 git remote prune origin --dry-run
 git remote prune origin 
 
 # delete branch
-git checkout master
+git checkout main
 git branch -D feature_branch_name
 
-# update local repo
-git pull --all
-
-
-##########################
-## git stash ##
-##########################
-# tutorial https://www.youtube.com/watch?v=KLEDKgMmbBI
-
+# git stash
 git stash save "my messsage"
 git stash list
 git stash apply stash@{0}
@@ -55,9 +33,6 @@ git stash drop stash@{0}
 
 # delete all stashed items
 git stash clear
-
-# empty git commit
-git commit --allow-empty -m ""
 
 # clone one branch
 git clone -b go-scientist-graphql git@git-ssh.deepl.dev:deepl/dap/dap.git
